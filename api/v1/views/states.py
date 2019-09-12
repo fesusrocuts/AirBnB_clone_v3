1d5c317458a7818fdc0995792129d3321e127fd2
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-12 19:22:11 +0000

    [Fix] Uploading the states file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ This file sets the States view"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 
 
-@app_views.route('/states')
+@app_views.route('/states', methods=['GET'])
 def list_states():
     """ This view is for listing all the states"""
     lista = storage.all("State")
@@ -15,9 +15,23 @@ def list_states():
     return jsonify(ouch)
 
 
-@app_views.route('/states/<state_id>')
+@app_views.route('/states/<state_id>', methods=['GET'])
 def get_states(state_id):
     """ This view is for getting a specific state """
     estadito = storage.get("State", state_id)
+    if estadito is None:
+        return abort(404)
     estadito = estadito.to_dict()
     return jsonify(estadito)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """ This view allows to erase an state"""
+    estadito = storage.get("State", state_id)
+    if estadito is None:
+        return abort(404)
+    estadito.delete()
+    storage.save()
+    dictio = {}
+    return dictio, 200
