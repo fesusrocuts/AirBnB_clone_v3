62800da71d549597da2f87f8a80e2910ca8d3c26
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-16 02:59:53 +0000

    [Fix] Uploading CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 HBNB_API_HOST = getenv('HBNB_API_HOST', "0.0.0.0")
 HBNB_API_PORT = int(getenv('HBNB_API_PORT', 5000))
@@ -11,7 +12,7 @@ HBNB_API_PORT = int(getenv('HBNB_API_PORT', 5000))
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(self):
